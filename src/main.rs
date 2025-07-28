b017beb6c40640fe4608a5f9919602f8c433143e
Jubilio <jubiliomausse5@gmail.com>
2025-07-28 16:58:51 +0200

    feat: port scan

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+mod port;
+mod model;
+mod common_ports;
+
+use crate::port::scan_ports;
+use crate::model::Subdomain;
+
+fn main() {
+    let mut subdomain = Subdomain {
+        domain: "guirrugo.com".to_string(),
+        open_ports: Vec::new(),
+    };
+
+    subdomain = scan_ports(subdomain);
+
+    println!("{:?}", subdomain.open_ports);
+}
